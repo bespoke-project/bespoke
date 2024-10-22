39891fb9a04091f4843cf586824d3aa727ac38da
SiamakMehrnia <siamakme20@gmail.com>
2024-10-22 23:28:27 +0200

    Router,Pages,MainLayout

--- bespoke1-frontend/src/App.jsx
+++ bespoke1-frontend/src/App.jsx
@@ -4,22 +4,29 @@ import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import MainLayout from "./layout/Mainlayout";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Search from "./pages/Search";
+import Profile from "./pages/Profile";
+
+
+const router =createBrowserRouter(
+  createRoutesFromElements(
+   <Route path="/" element={<MainLayout/>}>
+    <Route index element={<Home/>}/>
+    <Route path="/login" element={<Login/>}/>
+    <Route path="/register" element={<Register/>}/>
+    <Route path="/Search" element={<Search/>}/>
+    <Route path="/profile" element={<Profile/>}/>
+    </Route>
+    
+  )
+)
 
 function App() {
-  return <>
-
-  {/* TODO: Test-Div löschen, wenn fertig getestet. */}
-  <div title="Dies kann alles weg. Hier erst nur Test (mit class)" className="text-center">
-  <p className="italic">Test: Hallo Welt. Diese Zeile kann dann weg.</p>
-  <p>Tailwind-Klassen funktionieren</p>
-  <p>DaiyUI-Elemente funktionieren.</p>
-  <button
-  className="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
-  Button
-</button>
-</div>
-
-  </>;
+  return <RouterProvider router={router}/>;
 }
 
 export default App;
