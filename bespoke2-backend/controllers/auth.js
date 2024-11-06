cc6dca5b021e721c032c65688a0130c90b706dca
samish24 <samishaddad@protonmail.com>
2024-11-06 11:52:50 +0100

    accout deletion

--- bespoke2-backend/controllers/auth.js
+++ bespoke2-backend/controllers/auth.js
@@ -1,15 +1,15 @@
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import User from '../models/User.js';
-import asyncHandler from '../utils/asyncHandler.js';
-import ErrorResponse from '../utils/ErrorResponse.js';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+import asyncHandler from "../utils/asyncHandler.js";
+import ErrorResponse from "../utils/ErrorResponse.js";
 
 // REGISTER
 export const signUp = asyncHandler(async (req, res, next) => {
   const { firstName, lastName, username, email, password } = req.body;
 
   const existingUser = await User.findOne({ where: { email } });
-  if (existingUser) throw new ErrorResponse('Email already exists', 409);
+  if (existingUser) throw new ErrorResponse("Email already exists", 409);
 
   const hash = await bcrypt.hash(password, 10);
   const newUser = await User.create({
@@ -30,25 +30,25 @@ export const signIn = asyncHandler(async (req, res, next) => {
 
   const existingUser = await User.findOne({
     where: { email },
-    attributes: ['id', 'password'],
+    attributes: ["id", "password"],
   });
-  if (!existingUser) throw new ErrorResponse('Email does not exist', 404);
+  if (!existingUser) throw new ErrorResponse("Email does not exist", 404);
 
   const match = await bcrypt.compare(password, existingUser.password);
-  if (!match) throw new ErrorResponse('Password is incorrect', 401);
+  if (!match) throw new ErrorResponse("Password is incorrect", 401);
 
   const token = jwt.sign({ uid: existingUser.id }, process.env.JWT_SECRET, {
-    expiresIn: '30m',
+    expiresIn: "30m",
   });
 
-  res.cookie('token', token, { maxAge: 1800000 });
-  res.send({ status: 'logged in' });
+  res.cookie("token", token, { maxAge: 1800000 });
+  res.send({ status: "logged in" });
 });
 
 // LOGOUT
 export const signOut = asyncHandler(async (req, res, next) => {
-  res.clearCookie('token');
-  res.send({ status: 'logged out' });
+  res.clearCookie("token");
+  res.send({ status: "logged out" });
 });
 
 // UPDATE USER INFO
@@ -74,12 +74,12 @@ export const updateUser = asyncHandler(async (req, res, next) => {
     });
 
     if (updatedCount === 0) {
-      throw new ErrorResponse('User not found', 404);
+      throw new ErrorResponse("User not found", 404);
     }
 
-    res.send({ status: 'User info updated', user: updatedUsers[0] });
+    res.send({ status: "User info updated", user: updatedUsers[0] });
   } catch (error) {
-    console.error('Update error:', error);
+    console.error("Update error:", error);
     next(error);
   }
 });
@@ -94,8 +94,8 @@ export const getUser = asyncHandler(async (req, res, next) => {
 export const deleteUser = asyncHandler(async (req, res, next) => {
   const user = await User.findByPk(req.uid);
 
-  if (!user) throw new ErrorResponse('User not found', 404);
+  if (!user) throw new ErrorResponse("User not found", 404);
 
   await user.destroy();
-  res.send({ status: 'User deleted' });
+  res.send({ status: "User deleted" });
 });
